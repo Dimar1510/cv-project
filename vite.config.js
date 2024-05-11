96f7f62d62cb296de0d8f8c5864a024c5c2243de
dimar1150 <158440383+Dimar1510@users.noreply.github.com>
2024-05-12 00:05:38 +0300

    updated vite config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['react-to-print'] // Add any other external modules if needed
+    }
+  }
 })
+

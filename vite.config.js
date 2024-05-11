50de9209c0f81ba58c32d4fda65e9ae61e25f339
dimar1150 <158440383+Dimar1510@users.noreply.github.com>
2024-05-12 00:17:34 +0300

    Revert "updated vite config"
    This reverts commit 96f7f62d62cb296de0d8f8c5864a024c5c2243de.

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['react-to-print'] // Add any other external modules if needed
-    }
-  }
 })
-
